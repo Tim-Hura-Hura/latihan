bf736b2e099e4402d01db94f456e632ab917434e
Fuad HD <fuadhdn@gmail.com>
2019-10-04 08:01:15 +0700

    Fungsi untuk menambahkan data jasa.

--- app/Http/Controllers/JasaController.php
+++ app/Http/Controllers/JasaController.php
@@ -17,4 +17,35 @@ public function index()
 		return view ('kasir/jasa',['data'=>$data]);   
  	}
 
+	 public function store(Request $request)
+    {	
+    	$jenis_jasa		= $request->jenis_jasa;
+    	$harga 			= $request->harga;
+
+		$messages = [
+    	'min' => 'Jenis Jasa Yang Anda Masukan Sudah Teradaftar',
+    	'required' => ':attribute wajib diisi'
+		];
+
+
+    	$data = DB::table('jasa')->where([['jenis_jasa',$jenis_jasa]])->get();
+
+    	if(count($data) > 0) {
+    		$this->validate($request,[
+           'jenis_jasa' => 'min:10000'
+                ],$messages);
+
+		return redirect('kasir_jasa'); 	
+	
+		}
+
+		else {
+	
+		DB::table('jasa')->insert(['jenis_jasa' => $jenis_jasa,'harga' => $harga]);
+		return redirect('kasir_jasa')->with(['info' => 'Data Berhasil Ditambahkan']); 	
+		}
+
+
+		 
+	}
 }
