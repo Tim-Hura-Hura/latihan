624a71e0b38c591d04b174d6877deb60f5338848
amelia nurul <amelianurul66@gmail.com>
2019-10-22 18:53:30 +0700

    Update BarangController.php
    tambah barang pada gudang

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -18,6 +18,42 @@ public function index()
 		return view ('gudang/barang',['data'=>$data,'stok'=>$stok,'stok2'=>$stok2]);   
  	}
 
+ public function store(Request $request)
+    {	
+    	$messages = [
+    	'min' => 'Nama Barang Yang Anda Masukan Sudah Teradaftar',
+    	'required' => ':attribute wajib diisi'
+		];
+
+
+    	$nama_barang	= $request->nama_barang;
+    	$jumlah			= $request->jumlah;
+    	$harga_beli		= $request->harga_beli;
+    	$harga_jual		= $request->harga_jual;
+    	$suplier		= $request->suplier;
+
+		
+    	$data = DB::table('barang')->where([['nama_barang',$nama_barang]])->get();
+
+    	if(count($data) > 0) {
+    		$this->validate($request,[
+           'nama_barang' => 'min:10000'
+                ],$messages);
+
+          return redirect('gudang_barang'); 	
+	
+		}
+
+		else {
+		
+		DB::table('barang')->insert(['nama_barang' => $nama_barang]);
+		DB::table('stok')->insert(['nama_barang' => $nama_barang,'jumlah' => $jumlah,'harga_beli' => $harga_beli,'harga_jual' => $harga_jual,'suplier' => $suplier]);
+		return redirect('gudang_barang')->with(['info' => 'Data Berhasil Ditambah']); 	
+		}
+
+	}
+
+
 
    
 }
